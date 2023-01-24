dc76bebbe191fe0d85cd71b91e7624e9bda0a889
Neyroph <116799727+Neyroph@users.noreply.github.com>
2023-01-24 17:41:17 +0100

    beug correction about the voc timing

--- tts.py
+++ tts.py
@@ -2,6 +2,8 @@ import os
 import requests
 import playsound
 import keys
+import time
+from mutagen.mp3 import MP3
 
 def createAudioSpeechFromText(text, language, voice, path) -> None:
     response = requests.get(f'http://api.voicerss.org/?key={keys.TTS_KEY}&hl={language}&v={voice}&src={text}&r=2&c=mp3')
@@ -11,6 +13,8 @@ def createAudioSpeechFromText(text, language, voice, path) -> None:
 def playAudioFile(path) -> None:
     with open(path, 'rb') as file:
         playsound.playsound(path)
+        audio = MP3("speech.mp3")
+        time.sleep(audio.info.length - 2)
         file.close()
 
 def removeAudioFile(path) -> None:
