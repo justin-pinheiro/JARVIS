f08e98e59f087cf13ba298522084dfb821cd1fa8
justin <justin.pinheiro@laposte.net>
2023-02-08 11:28:57 +0100

    added data to training data

--- nlp.py
+++ nlp.py
@@ -11,35 +11,53 @@ ner=nlp.get_pipe("ner")
 
 # training data
 TRAIN_DATA = [
-            #   ("Play the song Side To Side", {"entities": [(0, 7, "ORG")]}),
-              ("I reached Chennai yesterday.", {"entities": [(19, 28, "GPE")]}),
-              ("I recently ordered a book from Amazon", {"entities": [(24,32, "ORG")]}),
-              ("I was driving a BMW", {"entities": [(16,19, "PRODUCT")]}),
-              ("I ordered this from ShopClues", {"entities": [(20,29, "ORG")]}),
-              ("Fridge can be ordered in Amazon ", {"entities": [(0,6, "PRODUCT")]}),
-              ("I rented a camera", {"entities": [(12,18, "PRODUCT")]}),
-              ("I rented a tent for our trip", {"entities": [(12,16, "PRODUCT")]}),
-              ("I rented a screwdriver from our neighbour", {"entities": [(12,22, "PRODUCT")]}),
-              ("I repaired my computer", {"entities": [(15,23, "PRODUCT")]}),
-              ("I got my clock fixed", {"entities": [(16,21, "PRODUCT")]}),
-              ("I got my truck fixed", {"entities": [(16,21, "PRODUCT")]}),
-              ("Flipkart started it's journey from zero", {"entities": [(0,8, "ORG")]}),
-              ("I recently ordered from Max", {"entities": [(24,27, "ORG")]}),
-              ("Flipkart is recognized as leader in market",{"entities": [(0,8, "ORG")]}),
-              ("I recently ordered from Swiggy", {"entities": [(24,29, "ORG")]})
+              # ("Hello there", {"entities": [(0, 4, "GREETING")]}),
+              # ("Good morning", {"entities": [(0, 10, "GREETING")]}),
+              # ("Good morning Jarvis", {"entities": [(0, 10, "GREETING")]}),
+              # ("Greetings", {"entities": [(0, 8, "GREETING")]}),
+              # ("Greetings Jarvis", {"entities": [(0, 8, "GREETING")]}),
+              # ("Hiya", {"entities": [(0, 3, "GREETING")]}),
+              # ("Hey", {"entities": [(0, 2, "GREETING")]}),
+              # ("Hey Jarvis", {"entities": [(0, 2, "GREETING")]}),
+              # ("Hi everyone", {"entities": [(0, 10, "GREETING")]}),
+              # ("What's up", {"entities": [(0, 7, "GREETING")]}),
+              # ("Good day", {"entities": [(0, 8, "GREETING")]}),
+              # ("Hello", {"entities": [(0, 5, "GREETING")]}),
+              # ("Hello Jarvis", {"entities": [(0, 5, "GREETING")]}),
+              # ("Hi there", {"entities": [(0, 8, "GREETING")]}),
+              # ("Howdy", {"entities": [(0, 5, "GREETING")]}),
+              # ("Yo", {"entities": [(0, 2, "GREETING")]}),
+              # ("Hola", {"entities": [(0, 4, "GREETING")]}),
+              # ("Salutations", {"entities": [(0, 11, "GREETING")]}),
+              # ("Hi", {"entities": [(0, 2, "GREETING")]}),
+              # ("Hi Jarvis", {"entities": [(0, 2, "GREETING")]}),
+              # ("Good evening", {"entities": [(0, 12, "GREETING")]}),
+              # ("Good evening Jarvis", {"entities": [(0, 12, "GREETING")]}),
+              ("Play Back in Black", {"entities": [(5, 17, "MUSIC_NAME")]}),
+              ("Play the song Back in Black", {"entities": [(14, 26, "MUSIC_NAME")]}),
+              ("Play the song Back in Black from AC/DC", {"entities": [(14, 26, "MUSIC_NAME")]}),
+              ("Play Back in Black from AC/DC", {"entities": [(5, 17, "MUSIC_NAME")]}),
+              ("Launch Back in Black", {"entities": [(7, 19, "MUSIC_NAME")]}),
+              ("Launch Back in Black from AC/DC", {"entities": [(7, 19, "MUSIC_NAME")]}),
+              ("Play Bad Romance", {"entities": [(5, 15, "MUSIC_NAME")]}),
+              ("Play the song Bad Romance", {"entities": [(14, 24, "MUSIC_NAME")]}),
+              ("Launch Bad Romance", {"entities": [(7, 17, "MUSIC_NAME")]}),
+              ("Play Till I Collapse", {"entities": [(5, 19, "MUSIC_NAME")]}),
+              ("Play the song Till I Collapse", {"entities": [(14, 28, "MUSIC_NAME")]}),
+              ("Launch Till I Collapse", {"entities": [(7, 21, "MUSIC_NAME")]}),
+              ("Play Merry Christmas", {"entities": [(5, 19, "MUSIC_NAME")]}),
+              ("Play the song Merry Christmas", {"entities": [(14, 28, "MUSIC_NAME")]}),
+              ("Launch Merry Christmas", {"entities": [(7, 21, "MUSIC_NAME")]}),
               ]
-              
-# Adding labels to the `ner`
 
+# Adding labels to the `ner`
 for _, annotations in TRAIN_DATA:
   for ent in annotations.get("entities"):
-    ner.add_label(ent[2])
+      ner.add_label(ent[2])
 
 # Disable pipeline components you dont need to change
 pipe_exceptions = ["ner", "trf_wordpiecer", "trf_tok2vec"]
 unaffected_pipes = [pipe for pipe in nlp.pipe_names if pipe not in pipe_exceptions]
-
-
 
 # TRAINING THE MODEL
 with nlp.disable_pipes(*unaffected_pipes):
@@ -49,7 +67,6 @@ with nlp.disable_pipes(*unaffected_pipes):
 
     # shuufling examples  before every iteration
     random.shuffle(TRAIN_DATA)
-    losses = {}
     # batch up the examples using spaCy's minibatch
     batches = minibatch(TRAIN_DATA, size=compounding(4.0, 32.0, 1.001))
     for batch in batches:
@@ -59,9 +76,9 @@ with nlp.disable_pipes(*unaffected_pipes):
             texts, annotations = zip(*batch)
             nlp.update(
                         [example],
-                        losses=losses,
-                        drop=0.5,  # dropout - make it harder to memorise data
+                        losses={},
+                        drop=0.7,  # dropout - make it harder to memorise data
                     )
 
-doc = nlp("I was driving a Alto")
+doc = nlp("Play Bad Romance")
 print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
